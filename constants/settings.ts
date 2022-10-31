7b68347bf61e7c7a3732c635973cfb77c276fdf1
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-31 16:01:12 +0400

    feat: Добавил контанты и типы для них

---
+++ constants/settings.ts
@@ -0,0 +1,4 @@
+import { GameValueType } from "../types/types"
+
+export const countItems: GameValueType = [2, 3, 4, 5] as const
+export const itemsValue: GameValueType = ["A", "9", "19", "50", "99", "999"] as const
---
+++ types/types.ts
@@ -0,0 +1,3 @@
+
+
+export type GameValueType = readonly (string | number)[]

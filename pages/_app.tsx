879d0c632f11f0e12ea88be9e1ae6dc5d8cdfd99
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-31 14:38:12 +0400

    feat: Добавил _app

---
+++ pages/_app.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import { ThemeProvider } from "@emotion/react";
+import type { AppProps } from "next/app";
+import GlobalStyles from "../styles/global";
+import { baseTheme } from "../styles/theme";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <ThemeProvider theme={baseTheme}>
+        <Component {...pageProps} />
+      </ThemeProvider>
+      <GlobalStyles />
+    </>
+  );
+}

465d6ca646b67158e279c49e443be822d8ed41d2
Pavel Gordeev <pavelgq@yandex.ru>
2022-10-31 16:01:52 +0400

    feat: Вынес переиспользуемые стили

---
+++ styles/mixins.ts
@@ -0,0 +1,17 @@
+import { css } from "@emotion/react";
+
+export const centerContainer = css`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const rowContainer = css`
+  display: flex;
+`;
+
+export const columnContainer = css`
+  display: flex;
+  flex-direction: column;
+`;

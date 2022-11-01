9061e6ecba98b5b9dfb27a5a598f9f96b42911be
Pavel Gordeev <pavelgq@yandex.ru>
2022-11-01 18:52:56 +0400

    fix: Поправил типы

--- types/types.ts
+++ types/types.ts
@@ -4,3 +4,5 @@ import { itemsValue } from "../constants/settings";
 export type GameValueType = readonly (string | number)[]
 
 export type ItemTypes = typeof itemsValue[number];
+
+export interface GameItemType { value: string | number; id: number; coinPath: string}
